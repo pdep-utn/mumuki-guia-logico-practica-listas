ea73825e8640a31d831c769d75a09ea783f4d137
PalumboN <nahuel.palumbo@gmail.com>
2015-05-22 02:25:25 -0300

    Refactor tests

--- 001_competenciaDeSaltos/test.pl
+++ 001_competenciaDeSaltos/test.pl
@@ -4,37 +4,30 @@ puntajes(ruben,[3,5,3,8,3]).
 puntajes(roque,[7,10,10]).
 
 test(hernan_hizo_6_puntos_en_4) :-
-  puntajeCompetidor(hernan, 4, Puntaje),
-  assertion(Puntaje == 6).
-
+  puntajeCompetidor(hernan, 4, 6).
 
 test(julio_hizo_9_puntos_en_1) :-
-  puntajeCompetidor(julio, 1, Puntaje),
-  assertion(Puntaje == 9).
-
+  puntajeCompetidor(julio, 1, 9).
 
 test(ruben_hizo_5_puntos_en_2) :-
-  puntajeCompetidor(ruben, 2, Puntaje),
-  assertion(Puntaje == 5).
+  puntajeCompetidor(ruben, 2, 5).
 
 test(ruben_hizo_3_puntos_en_5) :-
-  puntajeCompetidor(ruben, 5, Puntaje),
-  assertion(Puntaje == 3).
+  puntajeCompetidor(ruben, 5, 3).
 
-
+  
 test(julio_esta_descalificado) :-
   competidorDescalificado(julio).
-
 
 test(hernan_no_esta_descalificado, fail) :-
   competidorDescalificado(hernan).
 
+test(roque_no_esta_descalificado, fail) :-
+  competidorDescalificado(roque).
 
-test(roque_no_esta_descalificado) :-
-  not(competidorDescalificado(roque)).
-
-test(roque_clasifica) :-
+  
+test(roque_clasifica, nondet) :-
   competidorClasifica(roque).
 
-test(ruben_no_clasifica) :-
-  not(competidorClasifica(ruben)).
+test(ruben_no_clasifica, fail) :-
+  competidorClasifica(ruben).
