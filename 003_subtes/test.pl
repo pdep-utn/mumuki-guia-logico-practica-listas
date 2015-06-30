84a34f3f05366f126b1328eb988560ebc1a0d2ca
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-30 14:24:56 -0300

    Marcando tests como nondet

--- 003_subtes/test.pl
+++ 003_subtes/test.pl
@@ -9,20 +9,20 @@ combinacion([once,miserere]).
 combinacion([pellegrini,diagNorte,nueveJulio]).
 combinacion([independenciaC,independenciaE]).
 
-test(peru_esta_en_la_a) :-
+test(peru_esta_en_la_a, [nondet]) :-
   estaEn(peru, a).
-test(once_esta_en_la_h) :-
+test(once_esta_en_la_h, [nondet]) :-
   estaEn(once, h).
 
 
-test(distancia_entre_peru_y_primeraJunta_es_3) :-
+test(distancia_entre_peru_y_primeraJunta_es_3, [nondet]) :-
   distancia(peru, primaraJunta, Distancia),
   assertion(Distancia == 3).
 
-test(pellegrini_independencia_misma_altura) :-
+test(pellegrini_independencia_misma_altura, [nondet]) :-
   mismaAltura(pellegrini, independencia).
 
-test(bolivar_pichincha_viaje_facil) :-
+test(bolivar_pichincha_viaje_facil,[nondet]) :-
   viajeFacil(bolivar, pichincha).
-test(lima_independenciaC_viaje_facil) :-
+test(lima_independenciaC_viaje_facil, [nondet]) :-
   viajeFacil(lima, independenciaC).
