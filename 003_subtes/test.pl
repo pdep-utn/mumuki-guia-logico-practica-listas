81a03b13cd66a8ef9fb240eaacb9dd2818da46e8
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-30 14:33:51 -0300

    Corrigiendo tests

--- 003_subtes/test.pl
+++ 003_subtes/test.pl
@@ -1,15 +1,19 @@
 test(peru_esta_en_la_a, [nondet]) :-
   estaEn(peru, a).
-test(once_esta_en_la_h, [nondet]) :-
-  estaEn(once, h).
-
+test(lacroze_esta_en_la_b, [nondet]) :-
+  estaEn(lacroze, b).
+test(retiro_esta_en_la_c, [nondet]) :-
+  estaEn(retiro, c).
 
 test(distancia_entre_peru_y_primeraJunta_es_4, [nondet]) :-
   distancia(peru, primeraJunta, Distancia),
   assertion(Distancia == 4).
 
-test(pellegrini_independencia_misma_altura, [nondet]) :-
-  mismaAltura(pellegrini, independencia).
+test(pellegrini_diagonalNorte_misma_altura, [nondet]) :-
+  mismaAltura(pellegrini, diagonalNorte).
+test(alem_catedral_misma_altura, [nondet]) :-
+  mismaAltura(alem, catedral).
+
 
 test(bolivar_pichincha_viaje_facil,[nondet]) :-
   viajeFacil(bolivar, pichincha).
