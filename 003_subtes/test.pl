af53ef4e4a8c0ca5e1c5952c57bd0c3e79602bc5
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-30 14:28:50 -0300

    Separando extra

--- 003_subtes/test.pl
+++ 003_subtes/test.pl
@@ -1,14 +1,3 @@
-linea(a,[plazaMayo,peru,lima,congreso,miserere,rioJaneiro,primeraJunta,nazca]).
-linea(b,[alem,pellegrini,callao,gardel,medrano,malabia,lacroze,losIncas,urquiza]).
-linea(c,[retiro,diagNorte,avMayo,independenciaC,plazaC]).
-linea(d,[catedral,nueveJulio,medicina,plazaItalia,carranza,congresoTucuman]).
-linea(e,[bolivar,independenciaE,pichincha,jujuy,boedo,varela,virreyes]).
-linea(h,[once,venezuela,humberto1ro,inclan,caseros]).
-combinacion([lima,avMayo]).
-combinacion([once,miserere]).
-combinacion([pellegrini,diagNorte,nueveJulio]).
-combinacion([independenciaC,independenciaE]).
-
 test(peru_esta_en_la_a, [nondet]) :-
   estaEn(peru, a).
 test(once_esta_en_la_h, [nondet]) :-
