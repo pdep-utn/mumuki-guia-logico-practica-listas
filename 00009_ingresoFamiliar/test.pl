0215023f5cb001fd3cc2db3159970a72e69c8621
julianberbel@gmail.com <julianberbel@gmail.com>
2016-06-06 11:28:42 -0400

    Mumuki Export on 2016-06-06 11:28:42 -0400

--- 00009_ingresoFamiliar/test.pl
+++ 00009_ingresoFamiliar/test.pl
@@ -2,4 +2,4 @@ test('si no tiene hijos, el ingreso familiar es el ingreso total de la persona')
   ingresoFamiliar(luisa, 2000).
 
 test('si tiene hijos, el ingreso familiar es el ingreso total de la persona mas el de sus hijos') :-
-  ingresoFamiliar(luisa, 3000).
+  ingresoFamiliar(roque, 3000).
