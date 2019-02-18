d2006e24ff03d4e58b700bc97c8dc03f5094f368
mmatos87@gmail.com <mmatos87@gmail.com>
2019-02-18 13:10:06 +0000

    Mumuki Export on 2019-02-18 13:10:06 +0000

--- 00003_subtes/hint.md
+++ 00003_subtes/hint.md
@@ -1,1 +1,2 @@
-* investigar los predicados `nth1/3` y `nth0/3`.
+* Investigar los predicados `nth1/3` y `nth0/3`.
+* ¡Abstraer para poder reutilizar lógica!
--- 00003_subtes/test.pl
+++ 00003_subtes/test.pl
@@ -4,6 +4,8 @@ test(lacroze_esta_en_la_b, [nondet]) :-
   estaEn(lacroze, b).
 test(retiro_esta_en_la_c, [nondet]) :-
   estaEn(retiro, c).
+test(retiro_no_esta_en_la_b, fail):-
+  estaEn(retiro, b).
 
 test(distancia_entre_peru_y_primeraJunta_es_5, [nondet]) :-
   distancia(peru, primeraJunta, Distancia),
@@ -17,9 +19,13 @@ test(pellegrini_diagNorte_misma_altura, [nondet]) :-
   mismaAltura(pellegrini, diagNorte).
 test(alem_catedral_misma_altura, [nondet]) :-
   mismaAltura(alem, catedral).
+test(peru_medrano_no_misma_altura, fail):-
+  mismaAltura(peru, medrano).
 
 
 test(bolivar_pichincha_viaje_facil,[nondet]) :-
   viajeFacil(bolivar, pichincha).
 test(lima_independenciaC_viaje_facil, [nondet]) :-
   viajeFacil(lima, independenciaC).
+test(humberto1ro_lacroze_no_viaje_facil, fail) :-
+  viajeFacil(humberto1ro, lacroze).
